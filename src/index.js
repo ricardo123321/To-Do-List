58ecc5dcd920d2ae86706b4b5361d873540b472f
Lilith <themrblacksapce@hotmail.com>
2020-09-30 11:24:03 -0600

    add form to create a to do list with its javascript logic

--- src/index.js
+++ src/index.js
@@ -5,13 +5,29 @@ const Project = function(name){
     todoItems = [];
 }
 
-const todoIs = (name, projectN) => {
+const todoIs = (name, projectN, description, dueDate, priority) => {
     this.name = name;
     this.projectN = projectN;
-    description = '';
-    dueDate = '';
-    priority = '';
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
 }
+
+const submitBtn = document.getElementById('submitToDo');
+
+submitBtn.addEventListener('click', () => {
+  const name = document.getElementById('name').value;
+  const projectN = document.getElementById('projectN').value;
+  const description = document.getElementById('description').value;
+  const dueDate = document.getElementById('dueDate').value;
+  const priority = document.getElementById('priority').value;
+  const projectE = projectList.find(obj => {
+    return obj.name === projectN
+  });
+  toDo = new todoIs(name, projectN, description, dueDate, priority);
+  projectE.todoItems.push(toDo);
+  localStorage.setItem('projectList', JSON.stringify(projectList));
+});
 
 const createP = (name) => {
   if(projectList.length > 0 && name === 'Default'){
